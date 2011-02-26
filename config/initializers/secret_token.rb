b7d93bb8c66925ed36e434a59718f832347abff6
Nick Hoffman <nick@deadorange.com>
2011-02-25 19:56:56 -0500

    Initial commit of an empty Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EatTheAlphabet::Application.config.secret_token = '5ebdcedbc0c33367c1c01766e6b3f8903b0b4cde748632025098ab1307874665a63fd7adb08dabead4037156a84a5d3d434677cfc0c4d1cee5bb9d445ee6e419'
